ff311d086b0b2d553cb16a5eafa547f500b853cf
arya tamkhisha <aryabazy@gmail.com>
2021-05-03 12:41:32 +0430

    Bugs

---
+++ src/model/cards/nonMonsterCard/NonMonsterCard.java
@@ -0,0 +1,14 @@
+package model.cards.nonMonsterCard;
+
+import model.cards.Card;
+
+public class NonMonsterCard extends Card {
+    protected String type ;
+    protected int speed ;
+    public void summon() {
+        //?
+    }
+    public void set() {
+        //?
+    }
+}
