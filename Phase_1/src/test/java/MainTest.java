338ad6a76101914ffcf2e20978ff2f4811edb3d2
BozorgmehrZia <bozorgmehr1381@gmail.com>
2021-06-22 18:54:31 +0430

    Modify Unit Tests

--- Phase_1/src/test/java/MainTest.java
+++ Phase_1/src/test/java/MainTest.java
@@ -25,22 +25,22 @@ public class MainTest {
     @Test
     public void testNavigate(){
         controller.navigate("menu enter Profile");
-        Assertions.assertEquals("please login first", output.toString());
+        Assertions.assertEquals("please login first\r\n", output.toString());
     }
     @Test
     public void testRegisterMenu(){
         Player.getPlayers().clear();
         controller.registerMenu.run("user create --username Ali --nickname AliJoon --password ALi1234");
-        Assertions.assertEquals("user created successfully!", output.toString());
+        Assertions.assertEquals("user created successfully!\r\n", output.toString());
         output.reset();
         controller.registerMenu.run("user create Ali --nickname AliJoon --password ALi1234");
-        Assertions.assertEquals("invalid command", output.toString());
+        Assertions.assertEquals("invalid command\r\n", output.toString());
         output.reset();
         controller.registerMenu.run("user create -u Ali -p ALi1234 -n AliJoon");
-        Assertions.assertEquals("user with username Ali already exists", output.toString());
+        Assertions.assertEquals("user with username Ali already exists\r\n", output.toString());
         output.reset();
         controller.registerMenu.run("user create -u Hassan -p ALi1234 -n AliJoon");
-        Assertions.assertEquals("user with nickname AliJoon already exists", output.toString());
+        Assertions.assertEquals("user with nickname AliJoon already exists\r\n", output.toString());
     }
     @Test
     public void testLogin(){
@@ -48,13 +48,13 @@ public class MainTest {
         Assertions.assertEquals("Ali", Player.getPlayers().get(0).getUsername());
         output.reset();
         controller.registerMenu.run("user login -u Alii -p ALi1234");
-        Assertions.assertEquals("Username and password didn’t match!", output.toString());
+        Assertions.assertEquals("Username and password didn’t match!\r\n", output.toString());
         output.reset();
         controller.registerMenu.run("user login -u Ali -p ALii1234");
-        Assertions.assertEquals("Username and password didn’t match!", output.toString());
+        Assertions.assertEquals("Username and password didn’t match!\r\n", output.toString());
         output.reset();
         controller.registerMenu.run("user login -u Ali -p ALi1234");
-        Assertions.assertEquals("user logged in successfully!", output.toString());
+        Assertions.assertEquals("user logged in successfully!\r\n", output.toString());
         Assertions.assertEquals(Menu.MAIN, Controller.currentMenu);
 
     }
@@ -71,7 +71,7 @@ public class MainTest {
         resetOutput();
         controller.scoreboardMenu = new ScoreboardMenu();
         controller.scoreboardMenu.showScoreboard("scoreboard show");
-        String expected = "1- HassanJoon: 30001- ZiaJoon: 30003- AliJoon: 20004- AryaJoon: 1000";
+        String expected = "1- HassanJoon: 3000\r\n1- ZiaJoon: 3000\r\n3- AliJoon: 2000\r\n4- AryaJoon: 1000\r\n";
         Assertions.assertEquals(expected, output.toString());
         controller.navigate("menu exit");
     }
@@ -84,17 +84,17 @@ public class MainTest {
         controller.profileMenu = new ProfileMenu();
         controller.setMenusCurrentUser();
         controller.profileMenu.run("profile change -n Oskol");
-        Assertions.assertEquals("nickname changed successfully!", output.toString());
+        Assertions.assertEquals("nickname changed successfully!\r\n", output.toString());
         Assertions.assertEquals("Oskol", controller.profileMenu.getCurrentPlayer().getNickname());
         resetOutput();
         controller.profileMenu.run("profile change -p -n 1381 -c 1381");
-        Assertions.assertEquals("current password is invalid", output.toString());
+        Assertions.assertEquals("current password is invalid\r\n", output.toString());
         resetOutput();
         controller.profileMenu.run("profile change -c ALi1234 -n ALi1234 -p");
-        Assertions.assertEquals("please enter a new password", output.toString());
+        Assertions.assertEquals("please enter a new password\r\n", output.toString());
         resetOutput();
         controller.profileMenu.run("profile change -p -n 1381 -c ALi1234");
-        Assertions.assertEquals("password changed successfully!", output.toString());
+        Assertions.assertEquals("password changed successfully!\r\n", output.toString());
         Assertions.assertEquals("1381", controller.profileMenu.getCurrentPlayer().getPassword());
         controller.navigate("menu exit");
     }
@@ -118,13 +118,13 @@ public class MainTest {
         controller.setMenusCurrentUser();
         deckMenu.run("deck create AliDeck");
         Assertions.assertEquals(deckMenu.getCurrentPlayer().getDecks().get(0).getName(), "AliDeck");
-        Assertions.assertEquals("deck created successfully!", output.toString());
+        Assertions.assertEquals("deck created successfully!\r\n", output.toString());
         resetOutput();
         deckMenu.run("deck delete AliDeck");
         Assertions.assertEquals(deckMenu.getCurrentPlayer().getDecks().size(), 0);
         resetOutput();
         deckMenu.run("deck set-activate AliDeck");
-        Assertions.assertEquals("deck with name AliDeck does not exist", output.toString());
+        Assertions.assertEquals("deck with name AliDeck does not exist\r\n", output.toString());
         deckMenu.run("deck create Zia");
         deckMenu.run("deck set-activate Zia");
         Assertions.assertEquals("Zia", deckMenu.getCurrentPlayer().getActiveDeck().getName());
